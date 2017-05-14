ca9044622a781925cd3e5eb646ef413853702be6
unknown <euphmike>
2017-05-14 19:19:09 -0400

    added functionality to send messages from username and barebones layout CSS

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -3,12 +3,18 @@
     var Message = {};
     var ref = firebase.database().ref().child("messages");
     var messages = $firebaseArray(ref);
+    Message.all = messages;
 
     Message.getByRoomId = function(roomId) {
 
         return $firebaseArray(ref.orderByChild('roomId').equalTo(roomId));
 
     }
+
+    Message.send = function (newMessage) {
+        messages.$add(newMessage);
+        newMessage.sentAt = firebase.database.ServerValue.TIMESTAMP;
+    };
 
       return Message;
   }
